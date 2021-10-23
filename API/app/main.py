bf39fdb14cd0634358b57ca9cded8ecbb3274531
CodeSugar <hi@gus.works>
2021-10-23 10:20:03 -0500

    part1

--- API/app/main.py
+++ API/app/main.py
@@ -1,19 +1,42 @@
 from typing import Optional
-
+import csv
 from fastapi import FastAPI
+
+#Lectura Datos Dummy
+file = open('./app/ejemplo.csv')
+csvreader = csv.reader(file)
+header = []
+header = next(csvreader)
+rows = []
+for row in csvreader:
+    rows.append(row)
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Nvl": "World"}
 
 #Niveles de contracargos por banco o procesador
 
+
 @app.get("/Dashboard")
 def read_root():
-    return {"Niveles de contracargos por banco o procesador": "World"}
+    return {
+        "Porcentaje_de_contracargos_por_banco_o_procesador": {
+            "OPENPAY" : "22",
+            "BBVA" : "25",
+            "Banamex" : "27",
+            "Otros" : "23"
+        },
+        "Porcentaje_de_contracargo_por_BIN": {
+            "231423" : "22",
+            "231424" : "25",
+            "231421" : "27",
+            "231429" : "23"
+        }
+    }
 
 
 @app.get("/items/{item_id}")
