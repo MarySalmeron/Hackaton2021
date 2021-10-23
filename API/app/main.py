addbb68ef95d094b4583ed0c97756d7c81b91d9f
Gustavo Robles <hi@gus.works>
2021-10-23 18:39:10 +0000

    server

--- API/app/main.py
+++ API/app/main.py
@@ -1,27 +1,15 @@
 from typing import Optional
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from app.pqsql_lib.sqlBd import Bd
 
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="./static"), name="static")
-
 
 @app.get("/")
 def read_root():
     return {"Nvl": "World"}
-
-@app.get("/page", response_class=HTMLResponse)
-async def home(request: Request):
-    data = {
-        "page": "Home page adfadsfads"
-    }
-    return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 #Niveles de contracargos por banco o procesador
 
