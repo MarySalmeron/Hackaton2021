f2fa2323361487b1124d4bdb45f3081fb387b0ed
Angel Ramírez <aramireza1506@alumno.ipn.mx>
2021-10-23 03:40:27 -0500

    Adds example for templates on app

--- API/app/main.py
+++ API/app/main.py
@@ -1,13 +1,26 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="./static"), name="static")
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/page", response_class=HTMLResponse)
+async def home(request: Request):
+    data = {
+        "page": "Home page adfadsfads"
+    }
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 #Niveles de contracargos por banco o procesador
 
@@ -19,3 +32,15 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+### ENDPOINTS para consultar Cosas del buró
+
+
+### Endpoints para cargar contracargos
+
+
+### Endpoints para autenticar
+
+
+### 
